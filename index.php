66d63858bfbfa0286eaffe4da2835631689c2200
ubuntu <legol.chen@gmail.com>
2019-12-22 01:29:11 -0800

    made vscode_hack extension work

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?hh // strict
 
 <<__EntryPoint>>
-function main():noreturn{
+async function main(): Awaitable<void> {
   // try http://192.168.101.202:10020/index.php?test=abcd in chrome
   // and you will see
   // jay1:Hello World!1111
@@ -11,7 +11,7 @@ function main():noreturn{
 
   var_dump($_GET); // Element 'foo' is string(1) "a"
   var_dump($_POST); // Element 'bar' is string(1) "b"
-  var_dump($_REQUEST); // Does not contain elements 'foo' or 'bar'
+  var_dump($_REQUEST); // Does not contain elements 'foo' or 'bar'    
 
   exit(0);
 }
